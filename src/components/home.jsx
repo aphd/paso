dc1e5deacb91ec02c4840313f707587116e6ee64
apierro <antonio.pierro@gmail.com>
2019-09-17 00:08:01 +0200

    Added home component

---
+++ src/components/home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Helmet } from "react-helmet";
+
+const Home = () => (
+    <main className="container">
+        <Helmet>
+            <title>SOLPAR</title>
+        </Helmet>
+    </main>
+);
+
+export default Home;
