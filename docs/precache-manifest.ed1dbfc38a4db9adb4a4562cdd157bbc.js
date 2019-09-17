29322345d9c204e53eab6135a631b56f60bf22a1
apierro <antonio.pierro@gmail.com>
2019-09-17 17:10:12 +0200

    building

---
+++ docs/precache-manifest.ed1dbfc38a4db9adb4a4562cdd157bbc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea9769e6e4ee478a18d694d9e303d10b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b2238fec969c99e662e9",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "f2644263568d8154e0f3",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "b2238fec969c99e662e9",
+    "url": "/static/js/2.e6bd90f1.chunk.js"
+  },
+  {
+    "revision": "f2644263568d8154e0f3",
+    "url": "/static/js/main.0dc0704e.chunk.js"
+  },
+  {
+    "revision": "5f372ea40ffeefd775e8",
+    "url": "/static/js/runtime~main.46103692.js"
+  }
+]);
