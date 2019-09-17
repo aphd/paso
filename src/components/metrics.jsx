458fa8d13addffa75400bdb50c0382fa2d9a901c
apierro <antonio.pierro@gmail.com>
2019-09-17 16:16:29 +0200

    updating metric component

--- src/components/metrics.jsx
+++ src/components/metrics.jsx
@@ -1,13 +1,32 @@
 import React, { Component } from "react";
+import { json2html } from "../utils/json2html";
 
 class Metrics extends Component {
-    render() {
-        return (
-            <div className="alert alert-primary" role="alert">
-                Loc: {JSON.stringify(this.props.metric)}
-            </div>
-        );
-    }
+    render = () => {
+        try {
+            const { Version, LOC, types } = this.props.metric;
+            return (
+                <React.Fragment>
+                    <div className="alert alert-primary" role="alert"></div>
+                    <table className="table table-hover">
+                        <tbody>
+                            <tr>
+                                <th>Version</th>
+                                <td>{Version || "Undefined"}</td>
+                            </tr>
+                            <tr>
+                                <th>Loc</th>
+                                <td>{LOC}</td>
+                            </tr>
+                            {json2html(types)}
+                        </tbody>
+                    </table>
+                </React.Fragment>
+            );
+        } catch {
+            return <React.Fragment></React.Fragment>;
+        }
+    };
 }
 
 export default Metrics;
