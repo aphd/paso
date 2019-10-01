b144919334caade810b0587d6a430711e43cab21
apierro <antonio.pierro@gmail.com>
2019-10-01 07:11:26 +0200

    Added function fix_code to fix error :no viable alternative at input '_}'

--- src/services/writer.mjs
+++ src/services/writer.mjs
@@ -9,13 +9,15 @@ export class Writer {
         fns.forEach(fn => {
             try {
                 this.setMetrics(fn);
-            } catch {}
+            } catch(error) {
+                console.error(fn);
+            }
         });
     }
 
     setMetrics(fn) {
         this.lines = fs.readFileSync(fn, "utf8").split("\n");
-        const code = this.lines.slice(3, -4).join('\n');
+        const code = this.fix_code(this.lines.slice(3, -4).join('\n'));
         let current_metrics = solParse(code);
         current_metrics['submission_date'] = this.lines[1].match(/\d+-\d+-\d+/)[0];
         current_metrics['bytecode'] = this.lines.slice(-3, -2)[0].length;
@@ -28,12 +30,16 @@ export class Writer {
         try {
             const opts = fs.existsSync(this.output) ? {header: false} : {};
             const csv = Parse.parse(this.metrics, opts);
-            fs.appendFile(this.output, csv, (err) => {
+            fs.appendFile(this.output, csv + '\n', (err) => {
                 if (err) throw err;
                 console.log(`The metrics was appended to: ${this.output}!`);
             });
         } catch (err) {
             console.error(err);
         }
+    }
+
+    fix_code(code)  {
+        return code.replace(/(_)\s*(})$/gm,'$1;$2').replace(/(_)\s*$/gm, '$1;');
     }
 }
