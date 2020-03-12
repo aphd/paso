cdbecf990d7a0f3225b585c5427009494673cafc
apierro <antonio.pierro@gmail.com>
2020-03-13 00:11:13 +0100

    building

---
+++ docs/precache-manifest.57efd9357b5ab2f41053922b4f92711e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3c4ea5126aede529b0c81832cefe76c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "143cd12dc247371f0539",
+    "url": "./static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "7e1bf8c1208efebbbfa2",
+    "url": "./static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "143cd12dc247371f0539",
+    "url": "./static/js/2.7de2f4a2.chunk.js"
+  },
+  {
+    "revision": "1a2cf84338f1cf2b4d830dc6d9618d33",
+    "url": "./static/js/2.7de2f4a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e1bf8c1208efebbbfa2",
+    "url": "./static/js/main.644e7d38.chunk.js"
+  },
+  {
+    "revision": "27b65979b5f320fa97cf",
+    "url": "./static/js/runtime-main.9420ebb0.js"
+  }
+]);
--- docs/static/js/bundle.js
+++ docs/static/js/bundle.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "ce899d3113143c605503";
+/******/ 	var hotCurrentHash = "70355ed14f66dae90435";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
