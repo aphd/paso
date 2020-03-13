2d14d8833c7bf445aea2c7666913c0a32a8f911d
apierro <antonio.pierro@gmail.com>
2020-03-13 11:53:32 +0100

    adding page not found component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import PasoGui from "./components/paso-gui/";
+import PageNotFound from "./components/page-not-found";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 export default class App extends Component {
@@ -9,7 +10,7 @@ export default class App extends Component {
                 <div className="container">
                     <Switch>
                         <Route path="/" component={PasoGui} exact />
-                        {/* <Route component={Error} /> */}
+                        <Route component={PageNotFound} />
                     </Switch>
                 </div>
             </Router>
